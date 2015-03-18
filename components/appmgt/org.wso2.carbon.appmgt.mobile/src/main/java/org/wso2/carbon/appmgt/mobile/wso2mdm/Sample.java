5465c55d66aaf9d99377e7211c5c5ae4566c8502
Chatura Dilan <dilan@dilan.me>
2015-03-18 11:04:32 +0530

    Changes to sample data

--- components/appmgt/org.wso2.carbon.appmgt.mobile/src/main/java/org/wso2/carbon/appmgt/mobile/wso2mdm/Sample.java
+++ components/appmgt/org.wso2.carbon.appmgt.mobile/src/main/java/org/wso2/carbon/appmgt/mobile/wso2mdm/Sample.java
@@ -3,5 +3,5 @@ package org.wso2.carbon.appmgt.mobile.wso2mdm;
 
 public class Sample {
 
-    public static final String SAMPLE_DEVICES_JSON = "[ { \"id\": \"12\", \"name\": \"My Device 1\", \"platform\": \"android\", \"platform_version\": \"4.0\", \"image\": \"https://localhost:9443/store/extensions/assets/mobileapp/resources/models/none.png\", \"model\": \"Nexus\", \"type\": \"tab\" }, { \"id\": \"11\", \"name\": \"My iPhone\", \"platform\": \"android\", \"platform_version\": \"4.0\", \"image\": \"https://localhost:9443/store/extensions/assets/mobileapp/resources/models/none.png\", \"model\": \"iPhone\", \"type\": \"phone\" } ]";
+    public static final String SAMPLE_DEVICES_JSON = "[ { \"id\": \"12\", \"name\": \"My Nexus\", \"platform\": \"android\", \"platform_version\": \"5.0\", \"image\": \"/store/extensions/assets/mobileapp/resources/models/none.png\", \"model\": \"Nexus\", \"type\": \"tab\" }, { \"id\": \"11\", \"name\": \"My iPhone\", \"platform\": \"ios\", \"platform_version\": \"8.0\", \"image\": \"/store/extensions/assets/mobileapp/resources/models/none.png\", \"model\": \"iPhone\", \"type\": \"phone\" } ]";
 }
