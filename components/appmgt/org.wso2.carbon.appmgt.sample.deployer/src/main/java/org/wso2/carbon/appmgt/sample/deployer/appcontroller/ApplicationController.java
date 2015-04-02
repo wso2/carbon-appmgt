5c8fd2cee50aa2671b391a58ef4daa2b3be4e4d5
khsushan <khsushan@gmail.com>
2015-04-02 22:44:36 +0530

    add sample deployer component

--- components/appmgt/org.wso2.carbon.appmgt.sample.deployer/src/main/java/org/wso2/carbon/appmgt/sample/deployer/appcontroller/ApplicationController.java
+++ components/appmgt/org.wso2.carbon.appmgt.sample.deployer/src/main/java/org/wso2/carbon/appmgt/sample/deployer/appcontroller/ApplicationController.java
@@ -20,6 +20,24 @@ import java.io.IOException;
 import java.net.URLEncoder;
 import java.util.concurrent.ConcurrentHashMap;
 
+/*
+*  Copyright (c) 2005-2014, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+*
+*  WSO2 Inc. licenses this file to you under the Apache License,
+*  Version 2.0 (the "License"); you may not use this file except
+*  in compliance with the License.
+*  You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing,
+* software distributed under the License is distributed on an
+* "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+* KIND, either express or implied.  See the License for the
+* specific language governing permissions and limitations
+* under the License.
+*/
+
 public class ApplicationController {
 
     final static Logger log = Logger.getLogger(ApplicationController.class.getName());
