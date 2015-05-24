53e47e0cd5a394763b56a36d6e6d626fc6560437
dinusha <dinusha@wso2.com>
2015-05-24 20:15:53 +0530

    Update store/publisher features to be installed by other products

---
+++ features/appmgt/org.wso2.carbon.appmgt.publisher.feature/src/main/resources/publisher/themes/appm/helpers/list-assets.js
@@ -0,0 +1,9 @@
+var resources=function(page,meta){
+    return{
+        js:['caramel.handlebars.client.js', '/logic/recent-assets.js','delete.asset.js', 'list-assets.js'],
+        css: ['webapp/styles.css']
+
+
+    }
+}
+
