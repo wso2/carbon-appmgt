2d6e32f1f391f0bb1e5ef9782b87d1235f5f17bd
lahiru <lahiru.himasha@gmail.com>
2015-06-11 12:32:41 +0530

    Changes after review

--- features/appmgt/org.wso2.carbon.appmgt.publisher.feature/src/main/resources/publisher/themes/appm/js/edit.asset.js
+++ features/appmgt/org.wso2.carbon.appmgt.publisher.feature/src/main/resources/publisher/themes/appm/js/edit.asset.js
@@ -466,31 +466,31 @@ $(function() {
 
 		return formData;
 	}
-	
-	
-	function createServiceProvider(){
-        	var sso_config = {};
-        	var provider_name  = $('#providers').val();
-        	var logout_url = $('#overview_logoutUrl').val();
-        	var idp_provider = $('#sso_idpProviderUrl').val();
-        	var app_name = $('#overview_name').val();
-        	var app_version = $('#overview_version').val();
-        	var app_transport = $('#overview_transports').val();
-        	var app_context = $('#overview_context').val();
-			var app_allowAnonymous=$('#overview_allowAnonymous').val();
-			var app_provider = $('#overview_provider').val();
 
-        	var claims = [];
-        	var index=0;
-        	var propertyCount = document.getElementById("claimPropertyCounter").value;
-		while(index < propertyCount){
-			var claim = $("#claimPropertyName"+index).val();
-			if(claim != null){
-				claims[claims.length] = claim;
-			}
-			index++;
-			
-		}
+
+    function createServiceProvider() {
+        var sso_config = {};
+        var provider_name = $('#providers').val();
+        var logout_url = $('#overview_logoutUrl').val();
+        var idp_provider = $('#sso_idpProviderUrl').val();
+        var app_name = $('#overview_name').val();
+        var app_version = $('#overview_version').val();
+        var app_transport = $('#overview_transports').val();
+        var app_context = $('#overview_context').val();
+        var app_allowAnonymous = $('#overview_allowAnonymous').val();
+        var app_provider = $('#overview_provider').val();
+
+        var claims = [];
+        var index = 0;
+        var propertyCount = document.getElementById("claimPropertyCounter").value;
+        while (index < propertyCount) {
+            var claim = $("#claimPropertyName" + index).val();
+            if (claim != null) {
+                claims[claims.length] = claim;
+            }
+            index++;
+
+        }
 
 		sso_config.provider = provider_name;
 		sso_config.logout_url = logout_url;
@@ -500,7 +500,7 @@ $(function() {
 		sso_config.app_verison = app_version;
 		sso_config.app_transport = app_transport;
 		sso_config.app_context = app_context;
-		sso_config.app_allowAnonymous=app_allowAnonymous;
+        sso_config.app_allowAnonymous = app_allowAnonymous;
 		sso_config.app_provider = app_provider;
 
         $.ajax({
