4bd189f12871099f7ecbac35917f8b663520ebd8
sajithar <sajithar@wso2.com>
2015-06-23 14:18:26 +0530

    .js for 'Edit' and 'Create New Version' buttons. JS formatted.

--- features/appmgt/org.wso2.carbon.appmgt.publisher.feature/src/main/resources/publisher/themes/mobileapp/helpers/left-nav.js
+++ features/appmgt/org.wso2.carbon.appmgt.publisher.feature/src/main/resources/publisher/themes/mobileapp/helpers/left-nav.js
@@ -1,77 +1,95 @@
+/*
+ * Copyright (c) 2015, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 var getTypeObj, breadcrumbItems;
-var deploymentManagement=require('/modules/deployment/deployment.manager.js').deploymentManagementModule();
-var deploymentManager=deploymentManagement.cached();
+var deploymentManagement = require('/modules/deployment/deployment.manager.js').deploymentManagementModule();
+var deploymentManager = deploymentManagement.cached();
+
+var server = require('store').server;
+var permissions = require('/modules/permissions.js').permissions;
+var config = require('/config/publisher.json');
+
+var log = new Log();
 
 breadcrumbItems = deploymentManager.getAssetData()
 
-var generateLeftNavJson = function(data, listPartial) {
-	
-	var currentTypeObj = getTypeObj(data.shortName);
-	
-    var leftNavItems = { leftNavLinks :
-        [
-           /*
-            {
-                           name : "Browse All",
-                           additionalClasses : "active",
-                           url : "/publisher/assets/" + data.shortName + "/"
-                       },*/
-           
-           /* {
-                name : "Add " + data.shortName + "",
-                iconClass : "icon-plus-sign-alt",
-                additionalClasses : (listPartial == "add-asset" ) ? "active" : null,
-                url : "/publisher/asset/" + data.shortName + ""
-            },
-            {
-                name: "Statistics",
-                iconClass: "icon-dashboard",
-                additionalClasses : (listPartial == "statistics" ) ? "active" : null,
-                url: "/publisher/assets/statistics/" + data.shortName + "/"
-            }*/
-        ]
-    };
+var generateLeftNavJson = function (data, listPartial) {
 
+	var leftNavItems = {leftNavLinks: []};
 
-    if(data.artifact){
-        leftNavItems = { leftNavLinks :
-            [
-               /*
-                {
-                                   name : "Browse All",
-                                   additionalClasses : "active",
-                                   url : "/publisher/assets/" + data.shortName + "/"
-                               },*/
-               
-//                {
-//                    name : "Overview",
-//                    iconClass : "icon-list-alt",
-//                    additionalClasses : (listPartial == "view-asset" ) ? "active" : null,
-//                    url : "/publisher/asset/operations/view/" + data.shortName + "/" + data.artifact.id + ""
-//                },
-//                {
-//                    name : "Edit",
-//                    iconClass : "icon-edit",
-//                     additionalClasses : (listPartial == "edit-asset" ) ? "active" : null,
-//                    url : "/publisher/asset/operations/edit/" + data.shortName + "/" + data.artifact.id + ""
-//                },
-//                {
-//                    name : "Life Cycle",
-//                    iconClass : "icon-retweet",
-//                     additionalClasses : (listPartial == "lifecycle-asset" ) ? "active" : null,
-//                    url : "/publisher/asset/operations/lifecycle/" + data.shortName + "/" + data.artifact.id + ""
-//                }
-            ]
-        };
-    }
-    return leftNavItems;
+	if (listPartial != 'view-mobileapp') {
+		return leftNavItems;
+	}
+
+	if (data.artifact == null) {
+		return leftNavItems;
+	}
+
+	var user = server.current(session);
+	var um = server.userManager(user.tenantId);
+	var createActionAuthorized = permissions.isAuthorized(user.username, config.permissions.webapp_create, um);
+	if (!createActionAuthorized) {
+		return leftNavItems;
+	}
+
+	var editEnabled = permissions.isEditPermitted(user.username, data.artifact.path, um);
+	if (user.hasRoles(["admin"])) {
+		editEnabled = true;
+	}
+	if (data.artifact.lifecycleState == "Published") {
+		editEnabled = false;
+	}
+
+	if (editEnabled) {
+		leftNavItems = {
+			leftNavLinks: [
+				{
+					name: "Edit",
+					iconClass: "icon-edit",
+					additionalClasses: (listPartial == "edit-asset" ) ? "active" : null,
+					url: "/publisher/asset/operations/edit/" + data.shortName + "/" + data.artifact.id,
+					isEditable: editEnabled
+				}
+			]
+		};
+	} else {
+		leftNavItems = {
+			leftNavLinks: [
+				{
+					name: "Edit",
+					iconClass: "icon-edit",
+					additionalClasses: (listPartial == "edit-asset" ) ? "active" : false,
+					url: "#",
+					title: "Edit action not permitted.",
+					isEditable: editEnabled
+				}
+			]
+		};
+	}
+	return leftNavItems;
+
 };
 
-getTypeObj = function(type){
-	for(item in breadcrumbItems){
+getTypeObj = function (type) {
+	for (item in breadcrumbItems) {
 		var obj = breadcrumbItems[item]
-		if(obj.assetType == type){
+		if (obj.assetType == type) {
 			return obj;
 		}
 	}
-}
+};
