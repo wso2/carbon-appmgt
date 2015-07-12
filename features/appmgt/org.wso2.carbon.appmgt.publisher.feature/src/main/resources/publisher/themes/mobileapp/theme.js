d93bd313f1f3df98fad786f414689356d2581285
sajithar <sajithar@wso2.com>
2015-07-12 07:00:23 +0530

    added 'for' Handlebars helper. format code

--- features/appmgt/org.wso2.carbon.appmgt.publisher.feature/src/main/resources/publisher/themes/mobileapp/theme.js
+++ features/appmgt/org.wso2.carbon.appmgt.publisher.feature/src/main/resources/publisher/themes/mobileapp/theme.js
@@ -20,7 +20,8 @@ var engine = caramel.engine('handlebars', (function () {
                             return;
                         }
                         file.open('r');
-                        Handlebars.registerPartial(prefix.substring(0, prefix.length - 4), file.readAll());
+                        Handlebars.registerPartial(prefix.substring(0, prefix.length - 4),
+                                                   file.readAll());
                         file.close();
                     }
                 })('', file);
@@ -31,8 +32,9 @@ var engine = caramel.engine('handlebars', (function () {
 
             Handlebars.registerHelper('compare', function (lvalue, rvalue, options) {
 
-                if (arguments.length < 3)
+                if (arguments.length < 3) {
                     throw new Error("Handlerbars Helper 'compare' needs 2 parameters");
+                }
 
                 operator = options.hash.operator || "==";
 
@@ -61,10 +63,12 @@ var engine = caramel.engine('handlebars', (function () {
                     'typeof': function (l, r) {
                         return typeof l == r;
                     }
-                }
+                };
 
-                if (!operators[operator])
-                    throw new Error("Handlerbars Helper 'compare' doesn't know the operator " + operator);
+                if (!operators[operator]) {
+                    throw new Error("Handlerbars Helper 'compare' doesn't know the operator "
+                                    + operator);
+                }
 
                 var result = operators[operator](lvalue, rvalue);
 
@@ -96,6 +100,116 @@ var engine = caramel.engine('handlebars', (function () {
                         return options.inverse(this);
                 }
             });
+
+            /*
+             * Registers 'for' block helper in Handlebars which mimics classical for-loop.
+             * The 'for' helper allows to iterate over an array. Inside the block,
+             * you can use 'this' to reference the element being iterated over
+             * and 'index' to get the index of the current iteration.
+             *
+             *
+             * Optional Parameters:
+             *     startIndex - First index to be iterated over in the loop.
+             *                      Should be zero or a positive integer. Default value is 0.
+             *     endIndex - Last index to be iterated over in the loop.
+             *                      Should be zero or a positive integer. Default value is (data.length - 1).
+             *     jump - Increment/decrement of each iteration.
+             *                      Should be a positive integer. Default value is 1.
+             *
+             * Usage:
+             *     {{#foreach items}}
+             *         <div>Index of {{this}} value is {{index}}</div>
+             *     {{/foreach}}
+             *
+             *     {{#foreach items startIndex="5" endIndex="1"}}
+             *         {{this}}
+             *     {{/foreach}}
+             *
+             *     {{#foreach items startIndex="2" endIndex="10" jump="2"}}
+             *         {{this}}
+             *     {{/foreach}}
+             */
+            Handlebars.registerHelper('for', function (data, options) {
+                if (!Array.isArray(data)) {
+                    throw new Error("Handlerbars helper 'for' needs an array to iterate. But found "
+                                    + toString.call(data));
+                }
+
+                var tmp;
+                var startIndex = 0;
+                tmp = options.hash['startIndex'];
+                if (tmp) {
+                    startIndex = parseInt(tmp);
+                    if (isNaN(startIndex) || (startIndex < 0)) {
+                        throw new Error("'startIndex' parameter of Handlerbars helper 'for' "
+                                        + "should be zero or a positive integer. But found " + tmp);
+                    }
+                }
+                var endIndex = data.length - 1;
+                tmp = options.hash['endIndex'];
+                if (tmp) {
+                    endIndex = parseInt(tmp);
+                    if (isNaN(endIndex) || (endIndex < 0)) {
+                        throw new Error("'endIndex' parameter of Handlerbars helper 'for' "
+                                        + "should be zero or a positive integer. But found " + tmp);
+                    }
+                }
+                var jump = 1;
+                tmp = options.hash['jump'];
+                if (tmp) {
+                    jump = parseInt(tmp);
+                    if (isNaN(jump) || (jump < 1)) {
+                        throw new Error("'jump' parameter of Handlerbars helper 'for' "
+                                        + "should be a positive integer. But found " + tmp);
+                    }
+                }
+
+                var buffer = "";
+                if (startIndex <= endIndex) {
+                    for (var i = startIndex; i <= endIndex; i += jump) {
+                        var item = data[i];
+                        if (isObject(item)) {
+                            item.index = i;
+                        } else {
+                            item = new ForLoopItem(item, i);
+                        }
+                        buffer += options.fn(item);
+                    }
+                } else {
+                    for (var i = startIndex; endIndex <= i; i -= jump) {
+                        var item = data[i];
+                        if (isObject(item)) {
+                            item.index = i;
+                        } else {
+                            item = new ForLoopItem(item, i);
+                        }
+                        buffer += options.fn(item);
+                    }
+                }
+                return buffer;
+            });
+
+            /*
+             * Utility function to check given value is an object or not.
+             * @return <code>true</code> iff value is an object and
+             *          <code>false</code> if value is a string or a number or boolean or an array or a symbol or null or undefined.
+             */
+            function isObject(value) {
+                return (value && ((typeof value) === 'object')
+                        && (toString.call(value) === '[object Object]'));
+            }
+
+            /*
+             * Helper object class for 'for' block helper
+             */
+            function ForLoopItem(value, index) {
+                this.value = value;
+                this.index = index;
+            }
+
+            ForLoopItem.prototype.toString = function () {
+                return String(this.value);
+            }
 
         }
     };
