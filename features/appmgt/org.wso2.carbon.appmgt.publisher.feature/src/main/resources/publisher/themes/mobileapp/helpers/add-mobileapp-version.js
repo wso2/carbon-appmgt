c661e40e15264daca4829d15e57588cebc18204f
sajithar <sajithar@wso2.com>
2015-08-12 03:11:14 +0530

    added necessary js fi.es with new paths formatting

--- features/appmgt/org.wso2.carbon.appmgt.publisher.feature/src/main/resources/publisher/themes/mobileapp/helpers/add-mobileapp-version.js
+++ features/appmgt/org.wso2.carbon.appmgt.publisher.feature/src/main/resources/publisher/themes/mobileapp/helpers/add-mobileapp-version.js
@@ -1,7 +1,8 @@
 var resources = function (page, meta) {
     return {
-        js: ['mobileapp/bootstrap-fileupload.min.js', 'mobileapp/jquery.fileupload.js',
-             'mobileapp/add.mobileapp.version.js'],
-        css: ['mobileapp/bootstrap-fileupload.min.css', 'mobileapp/fileupload-extras.css']
+        js: ['lib/fileupload/bootstrap-fileupload.min.js',
+             'lib/fileupload/jquery.fileupload.js',
+             'logic/add.mobileapp.version.js'],
+        css: ['lib/fileupload/bootstrap-fileupload.min.css', 'lib/fileupload/fileupload-extras.css']
     };
 };
