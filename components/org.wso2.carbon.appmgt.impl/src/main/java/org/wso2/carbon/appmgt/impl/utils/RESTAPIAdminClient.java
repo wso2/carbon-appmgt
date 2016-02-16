d3987526bb0a0ed43659c34771aa8cf38c040af0
sajithar <sajithar@wso2.com>
2016-02-17 00:14:54 +0530

    corrected 'catch specialized exceptions instead of generic java.lang.Exception' and added more descriptive error messages as per https://github.com/wso2/carbon-appmgt/pull/63/files#r51676614 & https://github.com/wso2/carbon-appmgt/pull/63/files#r51732272 discussions

--- components/org.wso2.carbon.appmgt.impl/src/main/java/org/wso2/carbon/appmgt/impl/utils/RESTAPIAdminClient.java
+++ components/org.wso2.carbon.appmgt.impl/src/main/java/org/wso2/carbon/appmgt/impl/utils/RESTAPIAdminClient.java
@@ -17,14 +17,22 @@
 package org.wso2.carbon.appmgt.impl.utils;
 
 import org.apache.axis2.AxisFault;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.wso2.carbon.appmgt.api.model.APIIdentifier;
 import org.wso2.carbon.appmgt.impl.dto.Environment;
 import org.wso2.carbon.appmgt.impl.template.APITemplateBuilder;
+import org.wso2.carbon.appmgt.impl.template.APITemplateException;
+import org.wso2.carbon.rest.api.stub.RestApiAdminAPIException;
 import org.wso2.carbon.rest.api.stub.RestApiAdminStub;
 import org.wso2.carbon.rest.api.stub.types.carbon.APIData;
 import org.wso2.carbon.utils.multitenancy.MultitenantConstants;
 
+import java.rmi.RemoteException;
+
 public class RESTAPIAdminClient extends AbstractAPIGatewayAdminClient {
+
+    private static final Log log = LogFactory.getLog(RESTAPIAdminClient.class);
 
 	private RestApiAdminStub restApiAdminStub;
 	private String qualifiedName;
@@ -55,9 +63,19 @@ public class RESTAPIAdminClient extends AbstractAPIGatewayAdminClient {
 			} else {
 				restApiAdminStub.addApiFromString(appConfig);
 			}
-		} catch (Exception e) {
-			throw new AxisFault("Error while adding new WebApp", e);
-		}
+        } catch (APITemplateException e) {
+            String msg = "Cannot build configuration string for the versioned web app";
+            log.error(msg, e);
+            throw new AxisFault(msg, e);
+        } catch (RemoteException e) {
+            String msg = "An error occurred when calling the RestApiAdmin service to add the versioned web app";
+            log.error(msg, e);
+            throw new AxisFault(msg, e);
+        } catch (RestApiAdminAPIException e) {
+            String msg = "An error occurred when adding the versioned web app via RestApiAdmin service";
+            log.error(msg, e);
+            throw new AxisFault(msg, e);
+        }
 	}
 
 	/**
@@ -76,9 +94,15 @@ public class RESTAPIAdminClient extends AbstractAPIGatewayAdminClient {
 				appData = restApiAdminStub.getApiByName(qualifiedName);
 			}
 			return appData;
-		} catch (Exception e) {
-			throw new AxisFault("Error while obtaining WebApp information from gateway", e);
-		}
+        } catch (RemoteException e) {
+            String msg = "An error occurred when calling the RestApiAdmin service to retrieve the versioned web app";
+            log.error(msg, e);
+            throw new AxisFault(msg, e);
+        } catch (RestApiAdminAPIException e) {
+            String msg = "An error occurred when retrieving the versioned web app via RestApiAdmin service";
+            log.error(msg, e);
+            throw new AxisFault(msg, e);
+        }
 	}
 
 	/**
@@ -96,9 +120,19 @@ public class RESTAPIAdminClient extends AbstractAPIGatewayAdminClient {
 			} else {
 				restApiAdminStub.updateApiFromString(qualifiedName, appConfig);
 			}
-		} catch (Exception e) {
-			throw new AxisFault("Error while updating WebApp", e);
-		}
+        } catch (APITemplateException e) {
+            String msg = "Cannot build configuration string for the updating versioned web app";
+            log.error(msg, e);
+            throw new AxisFault(msg, e);
+        } catch (RemoteException e) {
+            String msg = "An error occurred when calling the RestApiAdmin service to update the versioned web app";
+            log.error(msg, e);
+            throw new AxisFault(msg, e);
+        } catch (RestApiAdminAPIException e) {
+            String msg = "An error occurred when updating the versioned web app via RestApiAdmin service";
+            log.error(msg, e);
+            throw new AxisFault(msg, e);
+        }
 	}
 
 	/**
@@ -114,9 +148,15 @@ public class RESTAPIAdminClient extends AbstractAPIGatewayAdminClient {
 			} else {
 				restApiAdminStub.deleteApi(qualifiedName);
 			}
-		} catch (Exception e) {
-			throw new AxisFault("Error while deleting WebApp", e);
-		}
+        } catch (RemoteException e) {
+            String msg = "An error occurred when calling the RestApiAdmin service to delete the versioned web app";
+            log.error(msg, e);
+            throw new AxisFault(msg, e);
+        } catch (RestApiAdminAPIException e) {
+            String msg = "An error occurred when deleting the versioned web app via RestApiAdmin service";
+            log.error(msg, e);
+            throw new AxisFault(msg, e);
+        }
 	}
 
 	/**
@@ -134,9 +174,19 @@ public class RESTAPIAdminClient extends AbstractAPIGatewayAdminClient {
 			} else {
 				restApiAdminStub.addApiFromString(appConfig);
 			}
-		} catch (Exception e) {
-			throw new AxisFault("Error publishing non-versioned web app to the gateway", e);
-		}
+        } catch (APITemplateException e) {
+            String msg = "Cannot build configuration string for the non-versioned web app";
+            log.error(msg, e);
+            throw new AxisFault(msg, e);
+        } catch (RemoteException e) {
+            String msg = "An error occurred when calling the RestApiAdmin service to add the non-versioned web app";
+            log.error(msg, e);
+            throw new AxisFault(msg, e);
+        } catch (RestApiAdminAPIException e) {
+            String msg = "An error occurred when adding the non-versioned web app via RestApiAdmin service";
+            log.error(msg, e);
+            throw new AxisFault(msg, e);
+        }
 	}
 
 	/**
@@ -155,9 +205,19 @@ public class RESTAPIAdminClient extends AbstractAPIGatewayAdminClient {
 			} else {
 				restApiAdminStub.updateApiFromString(qualifiedNonVersionedWebAppName, appConfig);
 			}
-		} catch (Exception e) {
-			throw new AxisFault("Error while updating non-versioned web app in the gateway", e);
-		}
+        } catch (APITemplateException e) {
+            String msg = "Cannot build configuration string for the updating non-versioned web app";
+            log.error(msg, e);
+            throw new AxisFault(msg, e);
+        } catch (RemoteException e) {
+            String msg = "An error occurred when calling the RestApiAdmin service to update the non-versioned web app";
+            log.error(msg, e);
+            throw new AxisFault(msg, e);
+        } catch (RestApiAdminAPIException e) {
+            String msg = "An error occurred when updating the non-versioned web app via RestApiAdmin service";
+            log.error(msg, e);
+            throw new AxisFault(msg, e);
+        }
 	}
 
 	/**
@@ -173,9 +233,15 @@ public class RESTAPIAdminClient extends AbstractAPIGatewayAdminClient {
 			} else {
 				restApiAdminStub.deleteApi(qualifiedNonVersionedWebAppName);
 			}
-		} catch (Exception e) {
-			throw new AxisFault("Error while deleting non-versioned web app from the gateway", e);
-		}
+        } catch (RemoteException e) {
+            String msg = "An error occurred when calling the RestApiAdmin service to delete the non-versioned web app";
+            log.error(msg, e);
+            throw new AxisFault(msg, e);
+        } catch (RestApiAdminAPIException e) {
+            String msg = "An error occurred when deleting the non-versioned web app via RestApiAdmin service";
+            log.error(msg, e);
+            throw new AxisFault(msg, e);
+        }
 	}
 
 	/**
@@ -194,9 +260,14 @@ public class RESTAPIAdminClient extends AbstractAPIGatewayAdminClient {
 				appData = restApiAdminStub.getApiByName(qualifiedNonVersionedWebAppName);
 			}
 			return appData;
-		} catch (Exception e) {
-			throw new AxisFault(
-					"Error while obtaining non-versioned web app information from gateway", e);
-		}
+        } catch (RemoteException e) {
+            String msg = "An error occurred when calling the RestApiAdmin service to retrieve the non-versioned web app";
+            log.error(msg, e);
+            throw new AxisFault(msg, e);
+        } catch (RestApiAdminAPIException e) {
+            String msg = "An error occurred when retrieving the non-versioned web app via RestApiAdmin service";
+            log.error(msg, e);
+            throw new AxisFault(msg, e);
+        }
 	}
 }
