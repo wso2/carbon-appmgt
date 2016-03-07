f777dfaedc2831ec6d5de5628621f045fd148967
Jena <y.jena1@yahoo.com>
2016-03-07 18:25:43 +0530

    Fixed- search button not working for webapp and sites

--- features/org.wso2.carbon.appmgt.store.feature/src/main/resources/store/themes/store/js/appm/search.js
+++ features/org.wso2.carbon.appmgt.store.feature/src/main/resources/store/themes/store/js/appm/search.js
@@ -160,28 +160,39 @@ $(function () {
     $('#searchTxt').keypress(function (e) {
         if (e.keyCode == 13) {  // detect the enter key
             var searchTerm = $(this).val();
-            if (checkNonSpecial(searchTerm)) {
-                var searchSelect = $('#searchSelect').val();
-                e.stopPropagation();
-                e.preventDefault();
-                if (searchSelect !== "App") {
-                    searchTerm = searchSelect + ":" + "\"" + searchTerm + "\"";
-                }
-                var searchUrl = $('#searchUrl').val();
-                if (searchUrl.indexOf('type=webapp') > -1) {
-                    location.href =
-                    location.protocol + '//' + location.host + searchUrl + '&query=' + searchTerm;
-                } else if (searchUrl.indexOf('type=site') > -1) {
-                    location.href =
-                    location.protocol + '//' + location.host + searchUrl + '&query=' + searchTerm;
-                } else {
-                    location.href =
-                    location.protocol + '//' + location.host + searchUrl + '?query=' + searchTerm;
-                }
-            }
+            e.stopPropagation();
+            e.preventDefault();
+            searchAsset(searchTerm);
         }
     });
 
+    $('#searchBtn').click(function(e){
+        var searchTerm = $('#searchTxt').val();
+        e.stopPropagation();
+        e.preventDefault();
+        searchAsset(searchTerm);
+    });
+
+    function searchAsset(searchTerm) {
+        if (checkNonSpecial(searchTerm)) {
+            var searchSelect = $('#searchSelect').val();
+            if (searchSelect !== "App") {
+                searchTerm = searchSelect + ":" + "\"" + searchTerm + "\"";
+            }
+            var searchUrl = $('#searchUrl').val();
+            if (searchUrl.indexOf('type=webapp') > -1) {
+                location.href =
+                location.protocol + '//' + location.host + searchUrl + '&query=' + searchTerm;
+            } else if (searchUrl.indexOf('type=site') > -1) {
+                location.href =
+                location.protocol + '//' + location.host + searchUrl + '&query=' + searchTerm;
+            } else {
+                location.href =
+                location.protocol + '//' + location.host + searchUrl + '?query=' + searchTerm;
+            }
+        }
+
+    }
     function checkNonSpecial(value){
         var non_special_regex = /^[A-Za-z][A-Za-z0-9\s-]*$/;
         return non_special_regex.test(value);
