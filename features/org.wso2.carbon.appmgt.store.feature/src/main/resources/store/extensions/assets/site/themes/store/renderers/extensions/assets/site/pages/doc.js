af9fb58b46a6251e72a75c9c11f00dd3e3b397e0
Jena <y.jena1@yahoo.com>
2016-03-07 20:47:23 +0530

    Fixed- Documentations are not shown in the store

--- features/org.wso2.carbon.appmgt.store.feature/src/main/resources/store/extensions/assets/site/themes/store/renderers/extensions/assets/site/pages/doc.js
+++ features/org.wso2.carbon.appmgt.store.feature/src/main/resources/store/extensions/assets/site/themes/store/renderers/extensions/assets/site/pages/doc.js
@@ -1,25 +1,75 @@
+
 var render = function (theme, data, meta, require) {
 
     var log = new Log();
 
-    theme('2-column-right', {
-        title: 'InlineDocument',
+    theme('2-column-left', {
+        title: data.title,
         header: [
             {
                 partial: 'header',
                 context: data.header
-            },
-            {
-                partial: 'navigation',
-                context: {}
             }
         ],
-        body: [
+        leftColumn: [
             {
-                partial: 'doc',
-                context: data
+                partial: 'left-column',
+                context: {
+                    navigation: createLeftNavLinks(data),
+                    tags: data.tags,
+                    recentApps: require('/helpers/asset.js').formatRatings(data.recentAssets)
+                }
+            }
+        ],
+        search: [
+            {
+                partial: 'search',
+                context: {searchQuery:data.search.query,searchUrl:data.search.searchUrl}
+            }
+        ],
+        pageHeader: [
+            {
+                partial: 'page-header',
+                context: {
+                    title: "Document",
+                    sorting: null
+                }
+            }
+        ],
+        pageContent: [
+            {
+                partial:'doc',
+                context:data
             }
         ]
     });
 
 };
+
+function createLeftNavLinks(data) {
+    var context = caramel.configs().context;
+    var leftNavigationData = [
+
+    ];
+
+    if (data.navigation.showAllAppsLink) {
+        leftNavigationData.push({
+                                    active: true, partial: 'all-apps', url: context + "/assets/site"
+                                });
+        leftNavigationData.push({
+                                    active: false, partial: 'my-apps', url: context + "/extensions/assets/site/myapps"
+                                });
+    } else {
+        leftNavigationData.push({
+                                    active: true, partial: 'my-apps', url: context + "/extensions/assets/site/myapps"
+                                });
+    }
+
+    if (data.user) {
+        leftNavigationData.push({
+                                    active: false, partial: 'my-favorites', url: context
+                + "/assets/favouriteapps?type=site"
+                                });
+    }
+    return leftNavigationData;
+}
--- features/org.wso2.carbon.appmgt.store.feature/src/main/resources/store/extensions/assets/webapp/themes/store/renderers/extensions/assets/webapp/pages/doc.js
+++ features/org.wso2.carbon.appmgt.store.feature/src/main/resources/store/extensions/assets/webapp/themes/store/renderers/extensions/assets/webapp/pages/doc.js
@@ -1,27 +1,75 @@
 
 var render = function (theme, data, meta, require) {
-	
-	var log = new Log();
 
-    theme('2-column-right', {
-        title: 'InlineDocument',
+    var log = new Log();
+
+    theme('2-column-left', {
+        title: data.title,
         header: [
             {
-              partial:'header',
-              context:data.header
-            },
-            {
-                partial: 'navigation',
-                context: {}
+                partial: 'header',
+                context: data.header
             }
-        ] ,
-        body:[
+        ],
+        leftColumn: [
+            {
+                partial: 'left-column',
+                context: {
+                    navigation: createLeftNavLinks(data),
+                    tags: data.tags,
+                    recentApps: require('/helpers/asset.js').formatRatings(data.recentAssets)
+                }
+            }
+        ],
+        search: [
+            {
+                partial: 'search',
+                context: {searchQuery:data.search.query,searchUrl:data.search.searchUrl}
+            }
+        ],
+        pageHeader: [
+            {
+                partial: 'page-header',
+                context: {
+                    title: "Document",
+                    sorting: null
+                }
+            }
+        ],
+        pageContent: [
             {
                 partial:'doc',
                 context:data
             }
         ]
-        
     });
 
 };
+
+function createLeftNavLinks(data) {
+    var context = caramel.configs().context;
+    var leftNavigationData = [
+
+    ];
+
+    if (data.navigation.showAllAppsLink) {
+        leftNavigationData.push({
+                                    active: true, partial: 'all-apps', url: context + "/assets/webapp"
+                                });
+        leftNavigationData.push({
+                                    active: false, partial: 'my-apps', url: context + "/extensions/assets/webapp/myapps"
+                                });
+    } else {
+        leftNavigationData.push({
+                                    active: true, partial: 'my-apps', url: context + "/extensions/assets/webapp/myapps"
+                                });
+    }
+
+    if (data.user) {
+        leftNavigationData.push({
+                                    active: false, partial: 'my-favorites', url: context
+                + "/assets/favouriteapps?type=webapp"
+                                });
+    }
+    return leftNavigationData;
+}
