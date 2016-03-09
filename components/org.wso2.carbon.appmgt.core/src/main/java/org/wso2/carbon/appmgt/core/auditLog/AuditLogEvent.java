1b271517b185c7a5ae76ea2d023efd7b8fb5c090
visithamanujaya <visithamanujaya@gmail.com>
2016-03-09 10:50:35 +0530

    Audit Logs

---
+++ components/org.wso2.carbon.appmgt.core/src/main/java/org/wso2/carbon/appmgt/core/auditLog/AuditLogEvent.java
@@ -0,0 +1,122 @@
+
+/*
+*  Copyright (c) 2005-2017, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+*
+*  WSO2 Inc. licenses this file to you under the Apache License,
+*  Version 2.0 (the "License"); you may not use this file except
+*  in compliance with the License.
+*  You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing,
+* software distributed under the License is distributed on an
+* "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+* KIND, either express or implied.  See the License for the
+* specific language governing permissions and limitations
+* under the License.
+*/
+package org.wso2.carbon.appmgt.core.auditLog;
+
+import java.io.Serializable;
+
+/**
+ * Stores details of a log temporary.
+ */
+public class AuditLogEvent implements Serializable {
+
+    private String action;
+    private String username;
+    private String tenantId;
+    private String subjectId;
+    private String subject;
+    private Object beforeChange;
+    private Object afterChange;
+
+    /**
+     * Constructor of the class to create a instant of a bean.
+
+     * @param tenantId
+     * @param username
+     * @param action
+     * @param subject
+     * @param subjectId
+     * @param beforeChange
+     * @param afterChange
+     */
+    public AuditLogEvent(String tenantId, String username, String action, String subject, String subjectId,
+                         Object beforeChange,
+                         Object afterChange) {
+        this.username = username;
+        this.afterChange = afterChange;
+        this.action = action;
+        this.beforeChange = beforeChange;
+        this.subject = subject;
+        this.subjectId = subjectId;
+        this.tenantId = tenantId;
+
+    }
+
+    /**
+     * Default Constructor.
+     */
+    public AuditLogEvent() {
+
+    }
+
+    public String getTenantId() {
+        return tenantId;
+    }
+
+    public String getAction() {
+        return action;
+    }
+
+    public void setAction(String action) {
+        this.action = action;
+    }
+
+    public Object getBeforeChange() {
+        return beforeChange;
+    }
+
+    public void setBeforeChange(Object beforeChange) {
+        this.beforeChange = beforeChange;
+    }
+
+    public Object getAfterChange() {
+        return afterChange;
+    }
+
+    public void setAfterChange(Object afterChange) {
+        this.afterChange = afterChange;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getSubjectId() {
+        return subjectId;
+    }
+
+    public void setSubjectId(String subjectId) {
+        this.subjectId = subjectId;
+    }
+
+    public void setTenantId(String tenantId) {
+        this.tenantId = tenantId;
+    }
+}
--- features/org.wso2.carbon.appmgt.store.feature/src/main/resources/store/extensions/assets/webapp/apis/v1/subscription.js
+++ features/org.wso2.carbon.appmgt.store.feature/src/main/resources/store/extensions/assets/webapp/apis/v1/subscription.js
@@ -3,78 +3,59 @@ var resource = (function () {
     var SubscriptionService;
     var subsApi;
 
-    var log = new Log('subscription-api');
+    var log = new Log('unsubscription-api');
+    var audutLog = require('/modules/auditLog/logger.js');
 
     SubscriptionService = require('/extensions/assets/webapp/services/subscription.js').serviceModule;
     subsApi = new SubscriptionService.SubscriptionService();
 
-	subsApi.init(jagg, session);
+    subsApi.init(jagg, session);
 
-    var AuthService = require('/extensions/assets/webapp/services/authentication.js').serviceModule;
-    var authenticator = new AuthService.Authenticator();
+    AuthService = require('/extensions/assets/webapp/services/authentication.js').serviceModule;
+    authenticator = new AuthService.Authenticator();
     authenticator.init(jagg, session);
 
-    
 
-    /*
-     Subscribes the given application to an API with the provided details
-     */
-    var addSubscription = function (context) {
 
-        if(authenticator.getLoggedInUser() == null){
-            response.sendRedirect(caramel.tenantedUrl('/login'));
-            return;
-        }
+
+    var removeSubscription = function (context) {
 
         var parameters = context.request.getAllParameters();
         var subscription = {};
+        var tenantId = session.get("tenantId");
+        var userName = session.get("LOGGED_IN_USER");
         subscription['apiName'] = parameters.apiName;
         subscription['apiVersion'] = parameters.apiVersion;
         subscription['apiTier'] = parameters.apiTier;
         subscription['apiProvider'] = parameters.apiProvider;
         subscription['appName'] = parameters.appName;
-        subscription['subscriptionType'] = parameters.subscriptionType;
-        subscription['enterprises'] = '';
-
-        if(subscription['subscriptionType'] == 'ENTERPRISE'){
-            subscription['enterprises'] = parameters.enterprises;
-        }
-
         subscription['user'] = authenticator.getLoggedInUser().username;
 
         log.info('Trying to add a subscription');
 
-        var result = subsApi.addSubscription(subscription);
-
+        var result = subsApi.removeSubscription(subscription);
         if(result){
-            subscription['op_type'] = 'ALLOW';
+            subscription['op_type'] = 'DENY';
             result = subsApi.updateVisibility(subscription);
+            audutLog.writeLog(tenantId, userName, "UserUnSubscribed","Webapp","{" +
+                                                                              "providerName='" +  subscription['apiProvider'] + '\'' +
+                                                                              ", apiName='" +  subscription['apiName'] + '\'' +
+                                                                              ", version='" +  subscription['apiVersion'] + '\'' +
+                                                                              '}', "", "");
         }
+
         return result;
     };
 
-    /*
-     Returns all of the apis to which the provided app is subscribed to
-     */
-    var getSubscription = function (context) {
 
-        var uriMatcher = new URIMatcher(request.getRequestURI());
-        var URI = '/{context}/resources/{asset}/{version}/{resource}/{username}';
-        var isMatch = uriMatcher.match(URI);
 
-        if (isMatch) {
-            var userName = uriMatcher.elements().username;
-            var applications = subsApi.getAppsWithSubs({user: userName});
-            return (applications && applications.length) ? applications[0].subscriptions : null;
-        } else {
-            return null;
-        }
-    };
 
 
     return{
-        post: addSubscription,
-        get: getSubscription
+        post: removeSubscription
+
+
+
     }
 
 })();
