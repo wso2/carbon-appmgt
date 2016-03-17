ede7c1c41a2ee80679c0f39372c47e4aac729cfc
Jena <y.jena1@yahoo.com>
2016-03-17 17:05:18 +0530

    Fix- search by provider is not working when @ sign is in user name

--- features/org.wso2.carbon.appmgt.store.feature/src/main/resources/store/extensions/assets/mobileapp/themes/store/js/search.js
+++ features/org.wso2.carbon.appmgt.store.feature/src/main/resources/store/extensions/assets/mobileapp/themes/store/js/search.js
@@ -16,19 +16,22 @@ $(function () {
     });
 
     function searchAsset(searchTerm) {
-        if (checkNonSpecial(searchTerm)) {
+        if (!checkIllegalChars(searchTerm)) {
             var searchSelect = $("#searchPane input[type='radio'][name='optionsRadios']:checked").val();
             searchTerm = searchSelect + ":" + "\"" + searchTerm + "\"";
             var searchUrl = $('#searchUrl').val();
 
             location.href =
             location.protocol + '//' + location.host + searchUrl + '?query=' + searchTerm;
+        } else {
+            alert("Search value  contains one or more illegal characters : [~!@#;%^*()+={}|\\<>\"\',]");
         }
     }
 
-    function checkNonSpecial(value) {
-        var non_special_regex = /^[A-Za-z][A-Za-z0-9\s-]*$/;
-        return non_special_regex.test(value);
+    function checkIllegalChars(value) {
+        // registry doesn't allow following illegal charecters
+        var special_regex = /[~!@#;%^*()+={}|\\<>"',]/;
+        return special_regex.test(value);
     }
 
 
