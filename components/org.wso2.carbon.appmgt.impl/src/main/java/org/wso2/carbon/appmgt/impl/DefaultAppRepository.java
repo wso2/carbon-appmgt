e31131db06d1fa59de80447eb2d74300383e11c2
tgtshanika <tgtshanika@gmail.com>
2016-05-11 11:08:38 +0530

    Add DefaultAppRepository constructor

--- components/org.wso2.carbon.appmgt.impl/src/main/java/org/wso2/carbon/appmgt/impl/DefaultAppRepository.java
+++ components/org.wso2.carbon.appmgt.impl/src/main/java/org/wso2/carbon/appmgt/impl/DefaultAppRepository.java
@@ -1,5 +1,7 @@
 package org.wso2.carbon.appmgt.impl;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.wso2.carbon.CarbonConstants;
 import org.wso2.carbon.appmgt.api.AppManagementException;
 import org.wso2.carbon.appmgt.api.model.App;
@@ -7,22 +9,44 @@ import org.wso2.carbon.appmgt.api.model.EntitlementPolicyGroup;
 import org.wso2.carbon.appmgt.impl.dao.AppMDAO;
 import org.wso2.carbon.appmgt.impl.service.ServiceReferenceHolder;
 import org.wso2.carbon.appmgt.impl.utils.AppManagerUtil;
+import org.wso2.carbon.context.CarbonContext;
 import org.wso2.carbon.registry.api.RegistryService;
 import org.wso2.carbon.registry.core.Registry;
 import org.wso2.carbon.registry.core.exceptions.RegistryException;
 import org.wso2.carbon.registry.core.session.UserRegistry;
+import org.wso2.carbon.user.api.UserStoreException;
 import org.wso2.carbon.utils.multitenancy.MultitenantUtils;
 
 /**
  * The default implementation of DefaultAppRepository which uses RDBMS and Carbon registry for persistence.
  */
-public class DefaultAppRepository implements AppRepository{
+public class DefaultAppRepository implements AppRepository {
 
 
     private RegistryService registryService;
+    private Registry registry;
+    private int tenantId;
+    private String tenantDomain;
+    private String username;
+    private Log log = LogFactory.getLog(getClass());
 
-    public DefaultAppRepository(){
-        this.registryService = ServiceReferenceHolder.getInstance().getRegistryService();
+    public DefaultAppRepository() throws AppManagementException {
+        try {
+            String loggedInUsername = CarbonContext.getThreadLocalCarbonContext().getUsername();
+
+            this.tenantDomain = MultitenantUtils.getTenantDomain(username);
+            this.username = MultitenantUtils.getTenantAwareUsername(loggedInUsername);
+            ;
+            this.tenantId = ServiceReferenceHolder.getInstance().getRealmService().getTenantManager().getTenantId(username);
+            AppManagerUtil.loadTenantRegistry(tenantId);
+            this.registry = ServiceReferenceHolder.getInstance().
+                    getRegistryService().getGovernanceUserRegistry(username, tenantId);
+
+        } catch (RegistryException e) {
+            handleException("Error while instantiating Registry for user : " + username, e);
+        } catch (UserStoreException e) {
+            handleException("Error while obtaining user registry for user:" + username, e);
+        }
     }
 
     @Override
@@ -55,5 +79,15 @@ public class DefaultAppRepository implements AppRepository{
     private void saveServiceProvider(App app){
 
     }
+    protected void handleException(String msg, Exception e) throws AppManagementException {
+        log.error(msg, e);
+        throw new AppManagementException(msg, e);
+    }
+
+    protected void handleException(String msg) throws AppManagementException {
+        log.error(msg);
+        throw new AppManagementException(msg);
+    }
+
 
 }
