ae34ef0479ccc48946af94d3dbbdefab732c3559
tgtshanika <tgtshanika@gmail.com>
2016-05-11 11:34:11 +0530

    Webapp artifact content create in DefaultAppRepository

--- components/org.wso2.carbon.appmgt.impl/src/main/java/org/wso2/carbon/appmgt/impl/DefaultAppRepository.java
+++ components/org.wso2.carbon.appmgt.impl/src/main/java/org/wso2/carbon/appmgt/impl/DefaultAppRepository.java
@@ -4,18 +4,23 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.wso2.carbon.CarbonConstants;
 import org.wso2.carbon.appmgt.api.AppManagementException;
-import org.wso2.carbon.appmgt.api.model.App;
-import org.wso2.carbon.appmgt.api.model.EntitlementPolicyGroup;
+import org.wso2.carbon.appmgt.api.model.*;
 import org.wso2.carbon.appmgt.impl.dao.AppMDAO;
 import org.wso2.carbon.appmgt.impl.service.ServiceReferenceHolder;
 import org.wso2.carbon.appmgt.impl.utils.AppManagerUtil;
 import org.wso2.carbon.context.CarbonContext;
+import org.wso2.carbon.governance.api.generic.GenericArtifactManager;
+import org.wso2.carbon.governance.api.generic.dataobjects.GenericArtifact;
+import org.wso2.carbon.governance.api.util.GovernanceUtils;
 import org.wso2.carbon.registry.api.RegistryService;
 import org.wso2.carbon.registry.core.Registry;
 import org.wso2.carbon.registry.core.exceptions.RegistryException;
 import org.wso2.carbon.registry.core.session.UserRegistry;
 import org.wso2.carbon.user.api.UserStoreException;
 import org.wso2.carbon.utils.multitenancy.MultitenantUtils;
+
+import javax.xml.namespace.QName;
+import java.util.*;
 
 /**
  * The default implementation of DefaultAppRepository which uses RDBMS and Carbon registry for persistence.
@@ -69,7 +74,11 @@ public class DefaultAppRepository implements AppRepository {
     }
 
     private String saveRegistryArtifact(App app){
-        return null;
+        String appUUID = null;
+        if(AppMConstants.WEBAPP_ASSET_TYPE.equals(app.getType())){
+            appUUID = saveWebAppRegistryArtifact((WebApp) app);
+        }
+
     }
 
     private long saveAppToRDMS(App app){
@@ -77,6 +86,58 @@ public class DefaultAppRepository implements AppRepository {
     }
 
     private void saveServiceProvider(App app){
+
+    }
+
+    private String saveWebAppRegistryArtifact(WebApp webApp) throws AppManagementException {
+        String artifactId = null;
+
+        GenericArtifactManager artifactManager = null;
+        try {
+            final String webAppName = webApp.getId().getApiName();
+            Map<String, List<String>> attributeListMap = new HashMap<String, List<String>>();
+
+            artifactManager = AppManagerUtil.getArtifactManager(registry,
+                    AppMConstants.WEBAPP_ASSET_TYPE);
+            registry.beginTransaction();
+            GenericArtifact genericArtifact =
+                    artifactManager.newGovernanceArtifact(new QName(webApp.getId().getApiName()));
+            GenericArtifact artifact = AppManagerUtil.createWebAppArtifactContent(genericArtifact, webApp);
+            artifactManager.addGenericArtifact(artifact);
+            artifactId = artifact.getId();
+            changeLifeCycleStatus(AppMConstants.WEBAPP_LIFE_CYCLE, artifactId, APPLifecycleActions.CREATE.getStatus());
+            String artifactPath = GovernanceUtils.getArtifactPath(registry, artifact.getId());
+
+            Set<String> tagSet = webApp.getTags();
+            if (tagSet != null) {
+                for (String tag : tagSet) {
+                    registry.applyTag(artifactPath, tag);
+                }
+            }
+            if (webApp.getAppVisibility() != null) {
+                AppManagerUtil.setResourcePermissions(webApp.getId().getProviderName(),
+                        AppMConstants.API_RESTRICTED_VISIBILITY, webApp.getAppVisibility(), artifactPath);
+            }
+            String providerPath = AppManagerUtil.getAPIProviderPath(webApp.getId());
+            //provider ------provides----> WebApp
+            registry.addAssociation(providerPath, artifactPath, AppMConstants.PROVIDER_ASSOCIATION);
+            registry.commitTransaction();
+        } catch (RegistryException e) {
+            try {
+                registry.rollbackTransaction();
+            } catch (RegistryException re) {
+                handleException(
+                        "Error while rolling back the transaction for web application: "
+                                + webApp.getId().getApiName(), re);
+            }
+            handleException("Error occurred while creating the web application : " + webApp.getId().getApiName(), e);
+        }
+        return artifactId;
+
+    }
+
+    private String saveMobileAppRegistryArtifact(MobileApp mobileApp){
+        return null;
 
     }
     protected void handleException(String msg, Exception e) throws AppManagementException {
