371b31d13360e7c297bbb4483c92c8488caad0f5
Thilini Shanika <tgtshanika@gmail.com>
2016-06-16 14:45:16 +0530

    APPM-1471:Self signup fails with below error fix

--- components/org.wso2.carbon.appmgt.impl/src/main/java/org/wso2/carbon/appmgt/impl/internal/AppManagerComponent.java
+++ components/org.wso2.carbon.appmgt.impl/src/main/java/org/wso2/carbon/appmgt/impl/internal/AppManagerComponent.java
@@ -145,6 +145,7 @@ public class AppManagerComponent {
 
             //load self sigup configuration to the registry
             AppManagerUtil.loadTenantSelfSignUpConfigurations(tenantId);
+            AppManagerUtil.createSelfSignUpRoles(tenantId);
             AppManagerUtil.loadTenantConf(tenantId);
             SignupObserver signupObserver = new SignupObserver();
             bundleContext.registerService(Axis2ConfigurationContextObserver.class.getName(), signupObserver,null);
