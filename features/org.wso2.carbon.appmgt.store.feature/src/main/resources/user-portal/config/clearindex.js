a0043c8adc6b8b6dc404dc2a56e7d649209c337a
danushkf <danushkaf@wso2.com>
2017-08-01 12:12:05 +0530

    merging two if statements
    (cherry picked from commit 3b33fa0e8ffa059cdb1a7d1a33e9367f25070418)

--- features/org.wso2.carbon.appmgt.store.feature/src/main/resources/user-portal/config/clearindex.js
+++ features/org.wso2.carbon.appmgt.store.feature/src/main/resources/user-portal/config/clearindex.js
@@ -7,16 +7,14 @@
     // 'sso_sessions' property is availabe only after the user signs in.
     // So we should proceed only if 'sso_sessions' is available.
     var sessionIndex = session.get("sessionIndex");
-    if (sso_sessions) {
-        if (sessionIndex) {
-            var httpSessionForSessionIndex = sso_sessions[sessionIndex];
-            if (httpSessionForSessionIndex) {
-                if (log.isDebugEnabled()) {
-                    log.debug("Deleting SSO session " + sessionIndex + " for the HTTP session " +
-                              httpSessionForSessionIndex.getId());
-                }
-                delete sso_sessions[sessionIndex];
+    if (sso_sessions && sessionIndex) {
+        var httpSessionForSessionIndex = sso_sessions[sessionIndex];
+        if (httpSessionForSessionIndex) {
+            if (log.isDebugEnabled()) {
+                log.debug("Deleting SSO session " + sessionIndex + " for the HTTP session " +
+                          httpSessionForSessionIndex.getId());
             }
+            delete sso_sessions[sessionIndex];
     	}
     }
 
