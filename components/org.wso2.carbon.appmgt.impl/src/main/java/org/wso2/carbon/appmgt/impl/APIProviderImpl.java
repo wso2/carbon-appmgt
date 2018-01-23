72ae0f47991b38d6204eb9551fd6d10b239e757f
Rushmin Fernando <rushfer@gmail.com>
2018-01-23 11:45:36 +0530

    APPM-1622 : Changing the RXT name for documentation to avoid conflicts with API Manager.

--- components/org.wso2.carbon.appmgt.impl/src/main/java/org/wso2/carbon/appmgt/impl/APIProviderImpl.java
+++ components/org.wso2.carbon.appmgt.impl/src/main/java/org/wso2/carbon/appmgt/impl/APIProviderImpl.java
@@ -1550,7 +1550,7 @@ class APIProviderImpl extends AbstractAPIManager implements APIProvider {
             }
 
             Association[] associations = registry.getAssociations(docPath,
-                                                                  AppMConstants.DOCUMENTATION_KEY);
+                                                                  AppMConstants.DOCUMENTATION_ASSOCIATION);
             for (Association association : associations) {
                 registry.delete(association.getDestinationPath());
             }
