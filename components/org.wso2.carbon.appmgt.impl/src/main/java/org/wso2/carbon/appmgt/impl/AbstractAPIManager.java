bcf83a9363b496caf8cc2dddb8302a19e1e1366a
Ace <ruwany.ace@gmail.com>
2018-02-22 12:22:18 +0530

    Re-enabling tags in appm for mobile apps and fixing related bugs

--- components/org.wso2.carbon.appmgt.impl/src/main/java/org/wso2/carbon/appmgt/impl/AbstractAPIManager.java
+++ components/org.wso2.carbon.appmgt.impl/src/main/java/org/wso2/carbon/appmgt/impl/AbstractAPIManager.java
@@ -117,7 +117,7 @@ public abstract class AbstractAPIManager implements APIManager {
             throws RegistryException, AppManagementException {
         String tagsQueryPath = RegistryConstants.QUERIES_COLLECTION_PATH + "/tag-summary-appmgt";
         String latestAPIsQueryPath = RegistryConstants.QUERIES_COLLECTION_PATH + "/latest-apis";
-        String resourcesByTag = RegistryConstants.QUERIES_COLLECTION_PATH + "/resource-by-tag";
+        String resourcesByTag = RegistryConstants.QUERIES_COLLECTION_PATH + "/resource-by-tag-appm";
         String path = RegistryUtils.getAbsolutePath(RegistryContext.getBaseInstance(),
                                                     RegistryConstants.GOVERNANCE_REGISTRY_BASE_PATH + "/repository/components/org.wso2.carbon.governance");
         if (username == null) {
