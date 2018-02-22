72198b13917d7633ae1a4c432470f8ebaf8710cb
Ace <ruwany.ace@gmail.com>
2018-02-22 12:55:50 +0530

    Fixing formatting issues

--- components/org.wso2.carbon.appmgt.impl/src/main/java/org/wso2/carbon/appmgt/impl/AbstractAPIManager.java
+++ components/org.wso2.carbon.appmgt.impl/src/main/java/org/wso2/carbon/appmgt/impl/AbstractAPIManager.java
@@ -119,7 +119,7 @@ public abstract class AbstractAPIManager implements APIManager {
         String latestAPIsQueryPath = RegistryConstants.QUERIES_COLLECTION_PATH + "/latest-apis";
         String resourcesByTag = RegistryConstants.QUERIES_COLLECTION_PATH + "/resource-by-tag-appm";
         String path = RegistryUtils.getAbsolutePath(RegistryContext.getBaseInstance(),
-                                                    RegistryConstants.GOVERNANCE_REGISTRY_BASE_PATH + "/repository/components/org.wso2.carbon.governance");
+                RegistryConstants.GOVERNANCE_REGISTRY_BASE_PATH + "/repository/components/org.wso2.carbon.governance");
         if (username == null) {
             try {
                 UserRealm realm = ServiceReferenceHolder.getUserRealm();
